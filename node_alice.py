f1a2f4f13289e438df5e8fc533947f42ddebec97
Udinanon <m.trapanotto@tutamail.com>
2019-11-05 13:46:44 +0100

    ora funziona Alice

--- node_alice.py
+++ node_alice.py
@@ -1,17 +1,21 @@
 from cqc.pythonLib import *
+import random
+def main():
+		
+	with CQCConnection("Alice") as Alice:
+		h_vector = [random.choice([0,1]) for _ in range(10)]
+		x_vector = [random.choice([0,1]) for _ in range(10)]
 
-with CQCConnection("Alice") as Alice:
+		q_vector = []
+		for i in range(10):
+			q_vector.append(qubit(Alice))
 
-    h_vector = [random.choice([0,1]) for _ in range(10)]
-    x_vector = [random.choice([0,1]) for _ in range(10)]
+		for i in range(10):
+			if x_vector[i] == 1:
+				q_vector[i].X()
+			if h_vector == 1:
+				q_vector[i].H()
 
-    q_vector = [quit(Alice) for _ in range(10)]
+		print(q_vector)
 
-    for i in range(10):
-        if x_vector[i] == 1:
-            q_vector[i].X()
-        if h_vector == 1:
-            q_vector[i].H()
-
-    print(q_vector)
-
+main()
