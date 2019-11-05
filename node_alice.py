b2682e61bed98cbced9326e2707bce242079a8e1
edoardobortolozzo <edoardobortolozzo@gmail.com>
2019-11-05 18:42:26 +0100

    non so piu' neanche io

--- node_alice.py
+++ node_alice.py
@@ -1,26 +1,71 @@
 from cqc.pythonLib import *
 import random
+import json
 def main():
-		
-	with CQCConnection("Alice") as Alice:
-		h_vector = [random.choice([0,1]) for _ in range(10)]
-		x_vector = [random.choice([0,1]) for _ in range(10)]
+        
+    with CQCConnection("Alice") as Alice:
+        n=10
 
-		q_vector = []
-		for i in range(10):
-			id_string = "100" + str(i)
-			q_vector.append(qubit(Alice))
+        hA_vector = [random.choice([0,1]) for _ in range(10)]
+        xA_vector = [random.choice([0,1]) for _ in range(10)]
+        hB_vector = [random.choice([0,1]) for _ in range(10)]
+        xB_vector = [random.choice([0,1]) for _ in range(10)]
 
-		for i in range(10):
-			id_string = "100" + str(i)
-			if x_vector[i] == 1:
-				q_vector[i].X()
-			if h_vector == 1:
-				q_vector[i].H()
+        qA_vector = []
+        qB_vector = []
+        for i in range(n):
+            qA_vector.append(qubit(Alice))
+            qB_vector.append(qubit(Alice))
 
-		for qbit in q_vector:
-			Alice.sendQubit(qbit, "Charlie")
-		print("gjgk")
-		charlie_output = Alice.recvClassical()
+        for i in range(n):
+            if xA_vector[i] == 1:
+                qA_vector[i].X()
+            if hA_vector == 1:
+                qA_vector[i].H()
+            if xB_vector[i] == 1:
+                qB_vector[i].X()
+            if hB_vector == 1:
+                qB_vector[i].H()
+
+        for qbit in qA_vector:
+            Alice.sendQubit(qbit, "Charlie")
+        print("gjgk")
+
+        for qbit in qB_vector:
+            Alice.sendQubit(qbit, "Charlie")
+        print("ACACA")
+
+        charlie_output=Alice.recvClassical()
+        M=json.loads(charlie_output.decode("utf-8"))
+
+        print("ha:", end="")
+        print(hA_vector)
+        print("hb:", end="")
+        print(hB_vector)
+        print("xa:", end="")
+        print(xA_vector)
+        print("xb:", end="")
+        print(xB_vector)
+        print()
+        for i in range(n):                
+            if M[i][0] == 1 or hA_vector[i] != hB_vector[i]:
+                xA_vector[i] = -1
+                #hA_vector[i] = -1
+                xB_vector[i] = -1
+                #hB_vector[i] = -1
+                continue
+            if M[i][1] == 1 and hA_vector[i] == 1:
+                continue
+            xA_vector[i] = 1 if xA_vector[i] == 0 else 0
+        
+        print("ha:", end="")
+        print(hA_vector)
+        print("hb:", end="")
+        print(hB_vector)
+        print("xa:", end="")
+        print(xA_vector)
+        print("xb:", end="")
+        print(xB_vector)
+
 
 main()
--- node_charlie.py
+++ node_charlie.py
@@ -5,20 +5,37 @@ import json
 
 def main():
     
+    n=10
     qA=[]
     qB=[]
 
-    with CQCConnection("Charlie") as CharlieA:
-        for i in range(10):
-            qI=CharlieA.recvQubit()
+    with CQCConnection("Charlie") as Charlie:
+        for i in range(n):
+            qI=Charlie.recvQubit()
             qA.append(qI)
-
-    with CQCConnection("Eve") as CharlieB:
-        for i in range(10):
-            qI=CharlieB.recvQubit()
+        
+        for i in range(n):
+            qI=Charlie.recvQubit()
             qB.append(qI)
+        
+        #-------------------------------------
+        #per ogni qubit fai cNOT , Alice.cNOT(BOB)
+        for i in range(len(qA)):
+            qA[i].cnot(qB[i])
+            
+        #Hadamard su array Alice
+        for i in range(len(qA)):
+            qA[i].H()
+
+        M_out=[]
+        for i in range(len(qA)):
+            M_out.append([qA[i].measure(), qB[i].measure()])
+        print(M_out)
+
+        Charlie.sendClassical("Alice", json.dumps(M_out).encode('utf-8'))
+#        Charlie.sendClassical("Alice", M_out)
+    #misura array Alice e Bob, Alice->vettore_successo, Bob->segno
+    #creo matrice risultati, [succ, segno]
     
-    print(qA)
-    print(qB)
 
 main()
